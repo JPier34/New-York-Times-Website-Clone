ae441be308d62f5d2bb38ac667951a85c7db429f
JPier34 <jacopo.pierantozzi@gmail.com>
2025-04-07 11:01:22 +0200

    og added, dependency fixings

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ArticlePage from "./pages/ArticlePage";
 import { ToastContainer } from "react-toastify";
-
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<><HomePage /></>} />
+                <Route path="/" element={<HomePage />} />
                 <Route path="/article/:articleId" element={<ArticlePage />} />
             </Routes>
             <ToastContainer />
